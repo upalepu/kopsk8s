e42a3f06256708406f22ab0ee703ecdbc3bc71cb
Uma Palepu <umap@crosslaketech.com>
2018-12-09 17:50:10 -0800

    Added pubkey,pvtkey

--- kopsk8s-vars.tf
+++ kopsk8s-vars.tf
@@ -1,7 +1,4 @@
-variable "aws_region" {
-	description = "AWS region to launch servers."
-	default     = "us-east-1"
-}
+variable "aws_region" { description = "AWS region to launch servers.", default = "us-east-1" }
 variable "project" { default = "k8s-demo" }
 variable "nodes" { description = "Number of worker nodes in k8s cluster. Default = 2", default = 2 }
 variable "nodetype" { description = "Worker node type (AWS machine type). Default = t2.micro", default = "t2.micro" }
@@ -13,3 +10,5 @@ variable "wink8sdir" { description = "Location for storing kops and kubectl. Use
 variable "comment" { description = "Comment for the hosted zone.", default = "Kubernetes cluster subdomain" }
 
 variable "userforcedestroy" { description = "Option to destroy forcefully.", default = "false" }
+variable "pvtkey_file" { description = "AWS private key file. Used by kops for creating the k8s cluster" }
+variable "pubkey_file" { description = "AWS public key file. Used by kops for creating the k8s cluster" }
